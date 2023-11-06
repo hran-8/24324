3b213bcf94acaf5ad97ca24159c5d61833559d66
H.L. <helloran0811@gmail.com>
2023-11-06 16:32:26 -0500

    updating everything to its most recent version

--- content/node-express-course/02-express-tutorial/app.js
+++ content/node-express-course/02-express-tutorial/app.js
@@ -1,1 +1,11 @@
-console.log('Express Tutorial')
+const http = require('http');
+
+const server = http.createServer((req, res)=>{
+    res.writeHead(200, {'content-type': 'text/plain'});
+    res.write('<h1>Home Page</h1>');
+    res.end();
+    
+
+});
+
+server.listen(5000);
