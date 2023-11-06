1962ff610975bf1eac3e05ac850f64b96e252182
Heyran L. <helloran0811@gmail.com>
2023-11-06 18:16:44 -0500

    added express part 1

--- content/node-express-course/02-express-tutorial/app.js
+++ content/node-express-course/02-express-tutorial/app.js
@@ -1,11 +1,31 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res)=>{
-    res.writeHead(200, {'content-type': 'text/plain'});
-    res.write('<h1>Home Page</h1>');
-    res.end();
-    
+
+app.get('/', (req, res)=>{
+    console.log('user hit the resource')
+    res.status(200).send('Home Page');
+
+})
+app.get('/about', (req, res)=>{
+    res.status(200).send('About Page');
+
+})
+
+app.all('*', (req, res)=>{
+    res.status(404).send('<h1>resource not found.</h1>');
+
+})
+
+app.listen(5000, ()=>{
+    console.log('server is listening on port 5000');
 
 });
 
-server.listen(5000);
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use 
+// app.listen
